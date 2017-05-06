570d7357f02fcca000f8f80a68a5b7aca7356a62
mennabakr <mennabakr26@yahoo.com>
2017-05-06 19:40:43 +0200

    myCodes
    This is my first C# codes

---
+++ sorted-names/sorted-names/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorted_names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[5];
+            int[] marks = new int[5];
+            int sum = 0;
+
+            Console.WriteLine("Please Enter Students Names and Marks : ");
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------");
+
+            for(int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine("Student {0} Name Is :", i + 1);
+                names[i] = Console.ReadLine();
+
+                Console.WriteLine("Student {0} Mark Is :", i + 1);
+                marks[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Student Names and Markes Table :");
+            Console.WriteLine();
+            Console.WriteLine("No\tName\tMark");
+            Console.WriteLine();
+
+            Array.Sort<string>(names);
+            for(int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", i + 1, names[i], marks[i]);
+                sum += marks[i];
+            }
+            Console.WriteLine("-----------------------");
+            Console.WriteLine();
+            Console.WriteLine("Sum\t\t{0}", sum);
+            Console.WriteLine();
+            Console.WriteLine("Average\t\t{0}",sum/(double)marks.Length);
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ students Project/students Project/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace students_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "student Averags";
+            int[] studentMarks = new int[5];
+            string[] studentNames = new string[5];
+            int sum = 0;
+
+            Console.WriteLine("Enter Students Names and Markes");
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------");
+            
+            for(int i = 0; i < studentMarks.Length; i++)
+            {
+                Console.WriteLine("Input Student {0} Name : ", i + 1);
+                studentNames[i] = Console.ReadLine();
+
+                Console.WriteLine("Input Student {0} Mark :", i + 1);
+                studentMarks[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine();
+            Console.WriteLine("Student Marks Table");
+            Console.WriteLine();
+            Console.WriteLine("----------------------");
+            Console.WriteLine("No\tName\tMark");
+            Console.WriteLine();
+
+            for (int i = 0; i < studentMarks.Length; i++)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", i + 1, studentNames[i], studentMarks[i]);
+                sum += studentMarks[i];
+            }
+            Console.WriteLine("----------------------");
+            Console.WriteLine();
+            Console.WriteLine("Sum\t\t{0}", sum);
+            Console.WriteLine("Average\t\t{0}", sum / (double)studentMarks.Length);
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
